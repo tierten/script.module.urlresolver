f65b3151c50ff83eb039e720c18023232240bfb0
Viper2k4 <Viper2k4@users.noreply.github.com>
2017-03-28 07:57:59 +0200

    just one import per line

--- lib/urlresolver/plugins/rapidvideocom.py
+++ lib/urlresolver/plugins/rapidvideocom.py
@@ -16,7 +16,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-import random, re
+import random
+import re
 from lib import helpers
 from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError

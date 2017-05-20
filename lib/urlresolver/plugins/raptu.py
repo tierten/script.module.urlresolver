54faa12bce71451f3e0e1e4fec6a8d1e6ddefb9c
alifrezser <darkraver001@gmail.com>
2017-05-20 10:59:23 +0200

    fix rapidvideocom, add raptu

---
+++ lib/urlresolver/plugins/raptu.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+    Kodi urlresolver plugin
+    Copyright (C) 2017 alifrezser
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from lib import helpers
+from urlresolver import common
+from urlresolver.resolver import UrlResolver, ResolverError
+
+class RaptuResolver(UrlResolver):
+    name = "raptu.com"
+    domains = ["raptu.com"]
+    pattern = '(?://|\.)(raptu\.com)/(?:[ev]/|embed/|\?v=)?([0-9A-Za-z]+)'
+
+    def __init__(self):
+        self.net = common.Net()
+
+    def get_media_url(self, host, media_id):
+        headers = {'User-Agent': common.RAND_UA}
+        html = self.net.http_GET(self.get_url(host, media_id), headers=headers).content
+        sources = helpers.parse_sources_list(html)
+        if sources:
+            sources = sorted(sources, key=lambda x: x[0])[::-1] 
+            return helpers.pick_source(sources)
+
+        raise ResolverError('File Not Found or removed')
+
+    def get_url(self, host, media_id):
+        return self._default_get_url(host, media_id, template='https://{host}/embed/{media_id}')
